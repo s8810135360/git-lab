088193a0650c27a60f90e35345b8089830778859
Jun-Ru Chang <jrjang@gmail.com>
2016-04-07 02:42:11 +0800

    fix test.c issues
    Signed-off-by: Jun-Ru Chang <jrjang@gmail.com>

--- test.c
+++ test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define LEN 7
 
@@ -9,33 +10,55 @@ int check1(char m)
 	if (m >= '0' && m <= '9')
 		return 0;
 
-	printf("Check 1 is fail! Please give 7 digit numbers!\n");
 	return 1;
 }
 
-int check2(char m)
+int check2(char *output)
 {
-	if (m == '\0')
-		return 0;
+	int length;
 
-	printf("Check 2 is fail! Over length!\n");
+	length = strlen(output);
+
+	if (length == LEN)
+		return 0;
+	else
+		printf("Check 2 is fail! Student id length is incorrect!\n");
+
 	return 1;
+}
+
+int check3(char *str1, char *str2)
+{
+	int ret;
+
+	ret = (strcmp(str1, str2) != 0);
+
+	if (ret)
+		printf("Check 3 is fail! Do not modify the return statement of getStudentID function!\n");
+
+	return ret;
 }
 
 int main()
 {
 	char id[10];
 	char *output;
-	int ret;
+	int ret = 0;
 
 	output = getStudentID(id);
 
 	/* check */
-	for (int i = 0; i <= LEN; i++) {
+
+	for (int i = 0; i < LEN; i++) {
 		ret |= check1(output[i]);
 	}
 
-	ret |= check2(output[LEN + 1]);
+	if (ret)
+		printf("Check 1 is fail! Please give 7 digit numbers!\n");
+
+	ret |= check2(output);
+
+	ret |= check3(output, id);
 
 	return ret;
 }
